8697adc2da66ff9f0d65feee59f63a3ae746f28f
Milad Rastin <miladrastin@Milads-MacBook-Air.local>
2025-12-31 02:25:02 -0500

    chore(components) : category slider

--- src/components/templates/home/category-slider.tsx
+++ src/components/templates/home/category-slider.tsx
@@ -1,9 +1,10 @@
 "use client"
+import Link from 'next/link';
 import { Swiper, SwiperSlide } from "swiper/react";
 import { FreeMode } from "swiper/modules";
 import { Badge } from "@/components/ui/badge";
 import Image from "next/image";
-import watchImage from "@/public/images/watch.png";
+import placeholderImg from "@/public/images/placeholder.webp";
 import type { ProductCategory } from "@/types";
 export default function CategorySlider({ categories } : { categories: ProductCategory[] }) {
     return (
@@ -19,15 +20,16 @@ export default function CategorySlider({ categories } : { categories: ProductCat
                 categories.map((category: ProductCategory) => {
                     return (
                         <SwiperSlide key={category.slug} className="w-fit!">
-                            <div className="w-fit border bg-transparent p-2 my-2" style={{ borderRadius: "999px 999px 100px 100px"}}>
+                            <Link href={`/products?category=${category.slug}`} 
+                            className="block w-fit border bg-transparent p-2 my-2" style={{ borderRadius: "999px 999px 100px 100px"}}>
                                 <div className="bg-white flex flex-col items-center justify-center p-2 gap-2" style={{ borderRadius: "999px 999px 100px 100px"}}>
                                     <div className="relative bg-[#F2DFCE] w-32 h-16 overflow-hidden p-2 mb-2" style={{ borderRadius: "999px 999px 100px 100px"}}>
-                                        <Image src={watchImage} alt="category" fill className="object-contain" />
+                                        <Image src={placeholderImg} alt="category" fill className="object-contain" />
                                     </div>
                                     <h2 className="text-sm">{category.name}</h2>
                                     <Badge variant="default">{category.slug}</Badge>
                                 </div>
-                            </div>  
+                            </Link>  
                         </SwiperSlide>
                     )
                 })
